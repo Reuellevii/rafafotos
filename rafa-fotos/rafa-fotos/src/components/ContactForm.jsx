2957e789050f45e5094ca6098d0b14c9f487b545
Reuellevii <paula.reuel60@gmail.com>
2026-09-23 14:22:08 -0300

    agrvai

--- rafa-fotos/rafa-fotos/src/components/ContactForm.jsx
+++ rafa-fotos/rafa-fotos/src/components/ContactForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { CONTATO } from '../config/content'
 import { NOME_DA_MARCA, INSTAGRAM_URL, CIDADE } from '../config/site'
 import { whatsappLink } from '../lib/whatsapp'
-import salvarLead from '../lib/leads'
+import { salvarLead } from '../lib/leads'
 import Reveal from './Reveal'
 import { IconInstagram, IconPin, IconWhatsApp } from './Icons'
 
